1c55f24b4f15f1dbd1b971615cddf7923ad16f8f
Nilton <yudtpb@gmail.com>
2020-04-16 21:23:10 -0400

    hello flutter

---
+++ lib/screens/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+          color: Colors.deepPurple,
+          child: Center(
+            child: Text(
+              sayHello() + ", Serena",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 36.0),
+            ),
+          ),
+        );
+  }
+  
+  String sayHello(){
+    String hello;
+    DateTime now = new DateTime.now();
+    int hour = now.hour;
+    int minute = now.minute;
+    if (hour < 12) {
+      hello = "Good Morning";
+    }
+    else if(hour <18){
+      hello = "Good Afternoon";
+    }
+    else {
+      hello = "Good Evening";
+    }
+    String minutes = (minute < 10) ? "0"+minute.toString() : minute.toString();
+
+    return "Agora é "+ hour.toString() + ":"+minutes + ". \n"+ hello;
+  }
+
+}
